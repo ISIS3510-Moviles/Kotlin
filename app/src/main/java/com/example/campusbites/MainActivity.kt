0a0872add81e5a87da2ce67eef7138872f390e3e
ds.rojass1 <ds.rojass1@uniandes.edu.co>
2025-02-19 17:14:38 -0500

    Material 3 added

--- app/src/main/java/com/example/campusbites/MainActivity.kt
+++ app/src/main/java/com/example/campusbites/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.campusbites.ui.theme.CampusBitesTheme
+import com.example.compose.CampusBitesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        color = MaterialTheme.colorScheme.primary,
         modifier = modifier
     )
 }
